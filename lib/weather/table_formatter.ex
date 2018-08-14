61062ca5fa8e780fdeafd7b5e72fda3df70044f6
Goh Matsumoto <urimaro@gmail.com>
2018-08-14 15:02:51 +0900

    Calculate column's widths

--- lib/weather/table_formatter.ex
+++ lib/weather/table_formatter.ex
@@ -1,11 +1,16 @@
 defmodule Weather.TableFormatter do
   def print_table_for_data(data, headers) do
     column_list = create_column_list(data, headers)
+    column_widths = width_of(column_list)
   end
 
   def create_column_list(data, headers) do
     Enum.zip(data, headers)
     |> Enum.map(fn x -> Tuple.to_list(x) end)
+  end
+
+  def width_of(column_list) do
+    for column <- column_list, do: column |> Enum.map(&String.length/1) |> Enum.max
   end
 end
 
